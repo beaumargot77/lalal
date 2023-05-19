182d4fa534c6b0afcd29026ea64cf600ac6c3fde
Margo <zaleskayamargarita@gmail.com>
2023-05-20 00:40:52 +0200

    add image

--- lalal/ContentView.swift
+++ lalal/ContentView.swift
@@ -10,15 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    
+                }
             }
+            .padding()
+            Image("IMG_7982")
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
     }
 }
 
