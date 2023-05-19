95f89cc4d9c9bf0152dcea749b5b3edf051993df
Margo <zaleskayamargarita@gmail.com>
2023-05-20 00:33:25 +0200

    Initial Commit

---
+++ lalal/lalalApp.swift
@@ -0,0 +1,17 @@
+//
+//  lalalApp.swift
+//  lalal
+//
+//  Created by Margarita Zaleskaya on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct lalalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
